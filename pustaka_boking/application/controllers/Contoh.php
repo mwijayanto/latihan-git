9c37fe4fbed5b0a0dcdb76f664a618ea642d32a9
mwijayanto <wijayanto.mohamad@gmail.com>
2023-10-10 20:01:56 +0700

    tugas pertemuan 2 dan 3

---
+++ pustaka_boking/application/controllers/Contoh.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh extends CI_Controller{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "nama saya Mohamad Wijayanto
+            saya tinggal di negara INDONESIAH";
+        //echo "Selamat Datang.. selamat belajar Web Programming";
+    }
+    
+} 
